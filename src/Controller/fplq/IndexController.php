3e4815d7a37b3f7176c8def9ba908b5a9b28f766
Steelboy78 <vinceamar@msn.com>
2019-01-08 17:36:54 +0100

    Correctif index

--- src/Controller/fplq/IndexController.php
+++ src/Controller/fplq/IndexController.php
@@ -8,19 +8,24 @@
 
 namespace App\Controller\fplq;
 
+
 use App\Entity\Restaurant;
 use App\Entity\Type;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Form\Extension\Core\Type\EmailType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class IndexController extends AbstractController
 {
     public function index()
     {
-        return $this->render('front/index.html.twig');
+        $repository = $this->getDoctrine()
+            ->getRepository(Restaurant::class);
+
+        $restaurants = $repository->findBy([]);
+
+        return $this->render('front/index.html.twig', [
+            'restaurants' => $restaurants
+        ]);
     }
 
     /**
@@ -37,14 +42,5 @@ class IndexController extends AbstractController
         return $this->render('front/ListeRestaurants.html.twig', [
             'restaurants' => $restaurants
         ]);
-    }
-
-    /**
-     * Page permettant d'afficher la liste des restaurant
-     * @Route("/menu", name="index_menu")
-     */
-    public function menu()
-    {
-        return $this->render('front/MenuRestaurants.html.twig');
     }
 }
