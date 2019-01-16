006c8af9068557747ad9d78e3154072b97506996
aissa <moaissa@outlook.fr>
2019-01-16 17:46:32 +0100

    boulotmomo

--- src/Controller/fplq/IndexController.php
+++ src/Controller/fplq/IndexController.php
@@ -71,7 +71,7 @@ class IndexController extends AbstractController
 
     /**
      * Page permettant d'afficher les restaurants
-     * @Route("membre/listerestaurants", name="index_restaurant")
+     * @Route("/listerestaurants", name="index_restaurant")
      */
     public function listerestaurants(Request $request)
     {
