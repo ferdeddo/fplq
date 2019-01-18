37d7c8802c293b9564f2ce9c0c48f871ee95c9a8
Steelboy78 <vinceamar@msn.com>
2019-01-18 12:28:30 +0100

    Update Final 5

--- src/Controller/fplq/Security/SecurityController.php
+++ src/Controller/fplq/Security/SecurityController.php
@@ -8,6 +8,7 @@
 
 namespace App\Controller\fplq\Security;
 
+use App\Form\ContactFormType;
 use App\Form\LoginFormType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -22,13 +23,48 @@ class SecurityController extends AbstractController
      * @param AuthenticationUtils $authenticationUtils
      * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function connexion( AuthenticationUtils $authenticationUtils)
+    public function connexion(Request $request, AuthenticationUtils $authenticationUtils, \Swift_Mailer $mailer)
     {
 
         # Création du Formulaire LoginFormType
         $form = $this->createForm(LoginFormType::class, [
             'email' => $authenticationUtils->getLastUsername()
         ]);
+
+        # Création du formulaire ContactFormType
+        $form_contact = $this->createForm(ContactFormType::class)
+            ->handleRequest($request);
+
+        # Soumission du Formulaire
+        if ($form_contact->isSubmitted() && $form_contact->isValid()) {
+
+            # Récupération des données pour SwiftMailer
+            $data = $form_contact->getData([]);
+
+            $message = (new \Swift_Message('Hello Email'))
+                ->setFrom($data['email'])
+                ->setTo('b3547af6c0-44c0b6@inbox.mailtrap.io')
+                ->setBody("Merci de bien vouloir prendre contact avec l'expediteur afin d'inscrire son restaurant en base de données !".
+                    '<br>'. $data['nom'].'<br>'.$data['prenom'].'<br>'.$data['email'].'<br>'.$data['tel'],
+//                    $this->renderView(
+//                    // templates/emails/registration.html.twig
+//                        'emails/registration.html.twig',
+//                        array('name' => $name)
+//                    ),
+                    'text/html'
+                );
+
+            $mailer->send($message);
+
+//          b3547af6c0-44c0b6@inbox.mailtrap.io
+
+            # Notification
+            $this->addFlash('notice_inscription',
+                'Félicitations, votre envoi a bien été validé, un commercial vous contactera très prochainement!');
+
+            # Redirection
+            return $this->redirectToRoute('security_connexion');
+        }
 
         # Récupération du message d'erreur
         $error = $authenticationUtils->getLastAuthenticationError();
@@ -42,7 +78,8 @@ class SecurityController extends AbstractController
 
         return $this->render('security/connexion.html.twig', [
             'form' => $form->createView(),
-//            'last_email' => $lastEmail,
+//              'last_email' => $lastEmail,
+            'form_contact' => $form_contact->createView(),
             'error' => $error
         ]);
 
